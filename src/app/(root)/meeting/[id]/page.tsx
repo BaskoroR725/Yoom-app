f84212ce761df8ef1df3f1fb6a98ee612e58c853
Baskoro Ramadhan <baskoror725@gmail.com>
2025-07-01 13:06:32 +0700

    finish sidebar and build navbar

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room: #{params.id} </div>;
+const Meeting = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  return <div>Meeting Room: # {id} </div>;
 };
 
 export default Meeting;
